fc4714565be05e595e2c41068ea18e09eab49f1c
Karthik Kanuganti <kanugantikarthikreddy@gmail.com>
2016-10-09 17:15:13 -0500

    Fix Importerror for sqlite3

--- crowdtimestory/upload/views.py
+++ crowdtimestory/upload/views.py
@@ -1,6 +1,5 @@
 import sqlite3
-from flask import Flask, request, session, g, redirect, url_for, \
-     abort, render_template, Blueprint, sqlite3
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, Blueprint
 import sys, os
 from werkzeug import secure_filename
 
